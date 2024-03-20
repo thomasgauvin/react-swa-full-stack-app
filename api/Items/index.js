0b341b14d2511748c9edc04b553553f40711ec8b
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2024-03-19 23:36:40 -0400

    remove mutations from app

--- api/Items/index.js
+++ api/Items/index.js
@@ -24,63 +24,64 @@ module.exports = async function (context, req) {
                 body: `Error retrieving item from the database: ${error.message}`
             };
         }
-    }
-    else if(req.method === "GET"){ //return all items
-        try {
-            const { resources } = await container.items.readAll().fetchAll();
-            context.res = {
-                status: 200,
-                body: resources
-            };
-        } catch (error) {
-            context.res = {
-                status: 500,
-                body: `Error retrieving items from the database: ${error.message}`
-            };
-        }
-    } else if(req.method === "POST"){ //create new item in the database
-        try {
-            const newItem = req.body;
-            const { resource: createdItem } = await container.items.create(newItem);
-            context.res = {
-                status: 201,
-                body: createdItem
-            };
-        } catch (error) {
-            context.res = {
-                status: 500,
-                body: `Error creating item in the database: ${error.message}`
-            };
-        }
-    } else if (req.method === "PUT") {
-        try {
-            const itemId = req.params.id;
-            const updatedItem = req.body;
-            const { resource: replacedItem } = await container.item(itemId, itemId).replace(updatedItem);
-            context.res = {
-                status: 200,
-                body: replacedItem
-            };
-        } catch (error) {
-            context.res = {
-                status: 500,
-                body: `Error updating item in the database: ${error.message}`
-            };
-        }
-    } else if (req.method === "DELETE") {
-        try {
-            const itemId = req.params.id;
-            await container.item(itemId, itemId).delete();
-            context.res = {
-                status: 204
-            };
-        } catch (error) {
-            console.log(error);
-            context.res = {
-                status: 500,
-                body: `Error deleting item from the database: ${error.message}`
-            };
-        }
+    // Uncomment mutations to have CRUD access, leaving READ access only for demo purposes
+    // } 
+    // else if(req.method === "GET"){ //return all items
+    //     try {
+    //         const { resources } = await container.items.readAll().fetchAll();
+    //         context.res = {
+    //             status: 200,
+    //             body: resources
+    //         };
+    //     } catch (error) {
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error retrieving items from the database: ${error.message}`
+    //         };
+    //     }
+    // } else if(req.method === "POST"){ //create new item in the database
+    //     try {
+    //         const newItem = req.body;
+    //         const { resource: createdItem } = await container.items.create(newItem);
+    //         context.res = {
+    //             status: 201,
+    //             body: createdItem
+    //         };
+    //     } catch (error) {
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error creating item in the database: ${error.message}`
+    //         };
+    //     }
+    // } else if (req.method === "PUT") {
+    //     try {
+    //         const itemId = req.params.id;
+    //         const updatedItem = req.body;
+    //         const { resource: replacedItem } = await container.item(itemId, itemId).replace(updatedItem);
+    //         context.res = {
+    //             status: 200,
+    //             body: replacedItem
+    //         };
+    //     } catch (error) {
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error updating item in the database: ${error.message}`
+    //         };
+    //     }
+    // } else if (req.method === "DELETE") {
+    //     try {
+    //         const itemId = req.params.id;
+    //         await container.item(itemId, itemId).delete();
+    //         context.res = {
+    //             status: 204
+    //         };
+    //     } catch (error) {
+    //         console.log(error);
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error deleting item from the database: ${error.message}`
+    //         };
+    //     }
     } else {
         context.res = {
             status: 405,
--- api/Sales/index.js
+++ api/Sales/index.js
@@ -22,62 +22,63 @@ module.exports = async function (context, req) {
                 body: `Error retrieving sale from the database: ${error.message}`
             };
         }
-    } else if (req.method === "GET") { // Return all sales
-        try {
-            const { resources } = await container.items.readAll().fetchAll();
-            context.res = {
-                status: 200,
-                body: resources
-            };
-        } catch (error) {
-            context.res = {
-                status: 500,
-                body: `Error retrieving sales from the database: ${error.message}`
-            };
-        }
-    } else if (req.method === "POST") { // Create new sale
-        try {
-            const newSale = req.body;
-            const { resource: createdSale } = await container.items.create(newSale);
-            context.res = {
-                status: 201,
-                body: createdSale
-            };
-        } catch (error) {
-            context.res = {
-                status: 500,
-                body: `Error creating sale in the database: ${error.message}`
-            };
-        }
-    } else if (req.method === "PUT") { // Update sale
-        try {
-            const saleId = req.params.id;
-            const updatedSale = req.body;
-            const { resource: replacedSale } = await container.item(saleId, saleId).replace(updatedSale);
-            context.res = {
-                status: 200,
-                body: replacedSale
-            };
-        } catch (error) {
-            context.res = {
-                status: 500,
-                body: `Error updating sale in the database: ${error.message}`
-            };
-        }
-    } else if (req.method === "DELETE") { // Delete sale
-        try {
-            const saleId = req.params.id;
-            await container.item(saleId, saleId).delete();
-            context.res = {
-                status: 204
-            };
-        } catch (error) {
-            console.log(error);
-            context.res = {
-                status: 500,
-                body: `Error deleting sale from the database: ${error.message}`
-            };
-        }
+    // Uncomment mutations to have CRUD access, leaving READ access only for demo purposes
+    // } else if (req.method === "GET") { // Return all sales
+    //     try {
+    //         const { resources } = await container.items.readAll().fetchAll();
+    //         context.res = {
+    //             status: 200,
+    //             body: resources
+    //         };
+    //     } catch (error) {
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error retrieving sales from the database: ${error.message}`
+    //         };
+    //     }
+    // } else if (req.method === "POST") { // Create new sale
+    //     try {
+    //         const newSale = req.body;
+    //         const { resource: createdSale } = await container.items.create(newSale);
+    //         context.res = {
+    //             status: 201,
+    //             body: createdSale
+    //         };
+    //     } catch (error) {
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error creating sale in the database: ${error.message}`
+    //         };
+    //     }
+    // } else if (req.method === "PUT") { // Update sale
+    //     try {
+    //         const saleId = req.params.id;
+    //         const updatedSale = req.body;
+    //         const { resource: replacedSale } = await container.item(saleId, saleId).replace(updatedSale);
+    //         context.res = {
+    //             status: 200,
+    //             body: replacedSale
+    //         };
+    //     } catch (error) {
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error updating sale in the database: ${error.message}`
+    //         };
+    //     }
+    // } else if (req.method === "DELETE") { // Delete sale
+    //     try {
+    //         const saleId = req.params.id;
+    //         await container.item(saleId, saleId).delete();
+    //         context.res = {
+    //             status: 204
+    //         };
+    //     } catch (error) {
+    //         console.log(error);
+    //         context.res = {
+    //             status: 500,
+    //             body: `Error deleting sale from the database: ${error.message}`
+    //         };
+    //     }
     } else {
         context.res = {
             status: 405,
